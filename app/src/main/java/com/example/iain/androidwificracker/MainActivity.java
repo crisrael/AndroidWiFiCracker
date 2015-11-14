e02318cbb256019a9b06c54841f161b61915fea8
ihlee001 <iainhlee@gmail.com>
2015-11-13 20:48:28 -0800

    final update for friday

--- app/src/main/java/com/example/iain/androidwificracker/MainActivity.java
+++ app/src/main/java/com/example/iain/androidwificracker/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.iain.androidwificracker;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -9,24 +13,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -49,5 +44,32 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void start_connection(View v){
+        int port;
+        EditText ip_input = (EditText)findViewById(R.id.ip_input);
+        EditText port_input = (EditText) findViewById(R.id.port_input);
+        String ip = ip_input.getText().toString();
+        if(port_input.getText().toString().equals("")) port = 0;
+        else port = Integer.parseInt(port_input.getText().toString());
+
+        connect(ip, port);
+/*        AlertDialog.Builder alert_builder = new AlertDialog.Builder(context).setMessage("ERROR");
+        alert_builder.setTitle("IP is not a suitable IP").setNeutralButton("Close",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        dialog.cancel();
+                    }
+                });
+        try{
+
+        } catch(NumberFormatException e){
+            AlertDialog alert = alert_builder.create();
+            alert.show();
+        }*/
+    }
+
+    public void connect(String ip, int port){
+        
     }
 }
